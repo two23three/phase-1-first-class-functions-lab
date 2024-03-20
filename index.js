bde23c0d415d70df58920890828219e2538e11e5
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-03-20 08:50:20 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+const drivers = ["Tulley","Jhon","verce","kukuli",];
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+    
+}
+const firstTwoDrivers=returnFirstTwoDrivers(drivers);
+console.log(firstTwoDrivers);
+
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+}
+const lastTwoDrivers=returnLastTwoDrivers(drivers)
+console.log(lastTwoDrivers);
+
+let selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+
+
+ const createFareMultiplier= function(multiplyValue){
+    return function (value){
+        return multiplyValue * value
+    }
+ }
+
+
+const fareDoubler = createFareMultiplier(2);
+
+
+const fareTripler=createFareMultiplier(3);
+const selectDifferentDrivers = function(drivers, driversToReturn) {
+    return driversToReturn(drivers);
+}
+
+    
+ 
